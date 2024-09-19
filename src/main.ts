576c3583d6f3c8465b32ae48fed2d35893b9835d
vsenadev <senavinicius01@gmail.com>
2024-09-19 11:34:45 -0300

    FIX - imports and modules fixed

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,6 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './App.module';
-// import * as dotenv from 'dotenv';
-//
-// dotenv.config();
-//
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//
-//   const globalPrefix = 'api';
-//   app.setGlobalPrefix(globalPrefix);
-//
-//   app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     res.setHeader(
-//       'Access-Control-Allow-Origin',
-//       '*' || process.env.CORS_ORIGIN,
-//     );
-//     res.setHeader(
-//       'Access-Control-Allow-Methods',
-//       'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-//     );
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-//     );
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end();
-//       return;
-//     }
-//     next();
-//   });
-//
-//   await app.listen(process.env.APPLICATION_PORT || 5000);
-// }
-//
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@src/App.module';
+import { AppModule } from './App.module';
 import * as dotenv from 'dotenv';
-import * as cors from 'cors';
 
 dotenv.config();
 
@@ -49,12 +10,28 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',
-    credentials: true,
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      '*' || process.env.CORS_ORIGIN,
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+    next();
   });
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.APPLICATION_PORT || 5000);
 }
 
 bootstrap();
